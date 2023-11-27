8a11db3d814dc1741b3a4f8a6378dde18aca655c
JasrajChouhan <jasrajc945@gmail.com>
2023-11-27 21:58:23 +0530

    create 2 btn for copy code and 2 btn for showing rgb and hashcode value

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
+let color;
+
+
+// hexcode to rgb convert
+function hashToRGB(hexColor) {
+    const patternColor = /^#([A-Fa-f0-9]{6})$/;
+
+    if (hexColor.match(patternColor)) {
+        const hexWithoutHash = hexColor.replace("#", "");
+        const r = parseInt(hexWithoutHash.substring(0, 2), 16);
+        const g = parseInt(hexWithoutHash.substring(2, 4), 16);
+        const b = parseInt(hexWithoutHash.substring(4, 6), 16);
+        return `RGB(${r}, ${g}, ${b})`;
+    } else {
+        return 'Error: Invalid Color Format';
+    }
+}
+
+//copy the value of rgb and hex code 
+function copyToClipboard(type) {
+    const textarea = document.getElementById('hiddenTextarea')
+    const value = (type === 'hash') ? color: hashToRGB(color)
+
+    textarea.value = value;
+    textarea.select();
+    document.execCommand('copy');
+    
+    // alert(`Copied ${type === 'hash' ? 'hash code' : 'RGB values'} to clipboard: ${value}`);
+}
+
+// create random colorcode in hex
 random = () =>{
-    let color = '#';
+    color = '#';
     const hex = '0123456789ABCDEF';
 
     for(let i = 0; i < 6; i++){
         color += hex[Math.floor(Math.random() * 16)];
         
     }
+
+    document.getElementById('copy-hash').addEventListener(
+        'click',
+        function (){
+            copyToClipboard('hash')
+
+        }
+        
+    )
+    document.getElementById('copy-rgb').addEventListener(
+        'click',
+        function (){
+            copyToClipboard('rgb')
+
+        }
+    )
+
+    const rgbValues = hashToRGB(color);
+
+    document.getElementById('hashcode').innerHTML = color;
+    document.getElementById('rgbcode').innerHTML = rgbValues;
+
     return color;
 }
 
@@ -29,8 +82,6 @@ stopTheChangeColor = () => {
     intervalId = null;
     
 }
-
-
 
 document.querySelector('#start').addEventListener(
     'click' ,
